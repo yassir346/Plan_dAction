dcbf15bdb23b08fbe241d6a10a377c9338e60020
yassir346 <yassir.mahir98@gmail.com>
2024-12-27 22:49:26 +0100

    fonction d'Ajouter une tache dans le BACKLOG avec JAVASCRIPT

--- Plan_dAction/Project/main.js
+++ Plan_dAction/Project/main.js
@@ -1,2 +1,22 @@
-const addButtonBacklog = document.getElementByIdelement('backlogADDbtn');
+const addButtonBacklog = document.getElementById('backlogADDbtn');
+const form = document.getElementById('form');
 
+addButtonBacklog.addEventListener("click", function(){
+    form.style = "display: flex; justify-content: center;";
+});
+
+const backlogAdd = document.getElementById("backlog-add");
+const submitTask = document.getElementById("submit-task");
+
+submitTask.addEventListener("click", function(){
+
+const titre = document.getElementById("titre").value;
+const description = document.getElementById("description").value;
+
+backlogAdd.insertAdjacentHTML("beforeend", `<div> ${titre} ${description}</div>`);
+
+document.getElementById("titre").value = '';
+
+form.style = "display: none;";
+
+});
